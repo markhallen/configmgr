65159ae65979f5dfd26c8edc837585caf2a800fb
Mark Allen <markhallen@outlook.com>
2016-11-01 11:52:26 +0100

    Updated opening comments to optimize for Get-Help

--- New-CMOperationalCollections/New-CMOperationalCollections.ps1
+++ New-CMOperationalCollections/New-CMOperationalCollections.ps1
@@ -1,19 +1,76 @@
-﻿# Description:  
-# Created:      18/10/2016
-# Author:       Mark Allen
-# Usage:        .\New-CMOperationalCollections.ps1 -SiteCode PR1 -Path \\%PathToXMLFile% -LimitingCollection "All Systems" -Organization "PST"
-# References:
-#       Benoit Lecours: Set Operational SCCM Collections https://gallery.technet.microsoft.com/Set-of-Operational-SCCM-19fa8178
+﻿<#
+.SYNOPSIS
+ Create collections in Configuration Manager that are useful for operational monitoring.
 
+ .DESCRIPTION
+ Will accept an XML file of the desired collections and queries and create the collections in Configuration Manager.
+
+.PARAMETER SiteCode
+ 3-character site code
+
+.PARAMETER Path
+ Full or relative path to the XML file containing the collections and queries
+
+.PARAMETER LimitingCollection
+ [Optional] Sets the LimitingCollection used for new collections.
+ [Default] "All Systems"
+
+.PARAMETER Organization
+ [Optional] allows a default top level directory to be defined. This is useful 
+ for a hierarchy that is shared between distinct departments or organizational units.
+ The value is also prepended to the collections names to allow each oraganisation to
+ create collections with unique names.
+ [Default] ""
+
+.PARAMETER FolderName
+ [Optional] This is the folder that will contain the operational collections. It will
+ be in the root of device collections or within the Organization folder if defined.
+ [Default] "Operational"
+
+.PARAMETER RecurInterval
+ [Optional] Used in conjunction with RecurCount to set a collection update schedule.
+ Acceptable values are 'Minutes','Hours' or 'Days'.
+ [Default] "Days"
+
+.PARAMETER RecurCount
+ [Optional] Used in conjunction with RecurInterval to set a collection update schedule.
+ [Default] 7
+
+.NOTES
+ Author: Mark Allen
+ Created: 18/10/2016
+ References: Benoit Lecours: Set Operational SCCM Collections 
+  https://gallery.technet.microsoft.com/Set-of-Operational-SCCM-19fa8178
+
+.EXAMPLE
+ .\New-CMOperationalCollections.ps1 -SiteCode PR1 -Path .\MyCollections.xml
+ Will create the folder 'Operational' in the root node Device Collections.
+ The collections will be created in the Operational Folder.
+ EG Device Collections > Operational > <Collections from XML>
+
+.EXAMPLE
+ .\New-CMOperationalCollections.ps1 -SiteCode PR1 -Path .\MyCollections.xml -LimitingCollection "All Systems" -Organization "MyOrg"
+ Will create a "Test" folder in the root node of each object type; subfolders will be
+ created within the relevant folder. Collection names will be prepended by <Organization>
+ EG Device Collections > MyOrg > Operational > <Collections from XML>
+    Collections will be prepended by "MyOrg ..."
+
+.EXAMPLE
+ .\New-CMOperationalCollections.ps1 -SiteCode PR1 -Path .\MyCollections.xml -RecurInterval "Days" -RecurCount "14"
+ A custom refesh interval will be set for all new collections.
+#>
 [CmdletBinding( SupportsShouldProcess = $False, ConfirmImpact = "None", DefaultParameterSetName = "" ) ]
 param(
  [Parameter(Mandatory=$true)]
  [string]$SiteCode,
+ [Parameter(Mandatory=$true)]
  [ValidateScript({Test-Path $(Split-Path $_) -PathType 'Container'})] 
  [string]$Path,
  [string]$LimitingCollection = 'All Systems',
  [String]$Organization = '',
  [String]$FolderName = 'Operational',
+ [Parameter()]
+ [ValidateSet('Minutes','Hours','Days')]
  [String]$RecurInterval = 'Days',
  [Int]$RecurCount = 7
 )
